1797816551624745f95236e7648d146dcedfcfc5
Gabriel Mazetto <brodock@gmail.com>
2009-04-07 19:37:19 -0300

    Reorganizando o código um pouco (removendo o código verboso do java e colocando imports nas classes geradas pelo IDLJ). Reformatado o códgio gerado pela IDLJ (em sua maioria)
    Adicionado servidor e ServicoEventosImpl

---
+++ src/ServicoEventosImpl.java
@@ -0,0 +1,37 @@
+
+import ServicoEventosPackage.listaEventosHolder;
+import org.omg.CORBA.Object;
+import org.omg.CORBA.StringHolder;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author brodock
+ */
+public class ServicoEventosImpl extends ServicoEventosPOA {
+
+    public boolean MeRegistre(Object ref, String evento) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void ObterListaEventos(listaEventosHolder lista) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public boolean NovoEvento(String evento) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public boolean CadastrarEvento(String evento) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public boolean obterEventoQualquer(StringHolder evento) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
---
+++ src/servidor/Servidor.java
@@ -0,0 +1,41 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servidor;
+
+import org.omg.CORBA.*;
+import org.omg.CORBA.Object;
+import org.omg.CosNaming.*;
+import org.omg.PortableServer.*;
+
+public class Servidor {
+
+    public static void main(String[] args) {
+        // Inicializa o ORB
+        ORB orb = ORB.init(args, null);
+        try {
+
+            // Ativa o POA
+            POA poa = POAHelper.narrow(orb.resolve_initial_references("RootPOA"));
+            poa.the_POAManager().activate();
+
+            // Instancia um objeto da classe GoodDayImpl
+            GoodDayImpl gd = new GoodDayImpl("aaa");
+
+            // Transforma o objeto java HelloWorld (hw) num objeto CORBA genérico (o)
+            Object objCORBA = poa.servant_to_reference(gd);
+
+            // Obtém a referência (endereço) do servidor de nomes (NameService)
+            // Essa tarefa é realizada pelo ORB (orb.resolve_initial_references)
+            NamingContextExt nc = NamingContextExtHelper.narrow(orb.resolve_initial_references("NameService"));
+
+            // Registra a referência objCORBA no servidor de nomes usando o bind (pode ser tb com rebind)
+            nc.rebind(nc.to_name("Hello.example"), objCORBA);
+
+            orb.run();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
