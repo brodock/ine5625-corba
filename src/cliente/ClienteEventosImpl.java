b29e5781766d374680e36f72709f7a366f937c9f
Gabriel Mazetto <brodock@gmail.com>
2009-04-10 22:41:55 -0300

    Adicionando implementação do lado do servidor CORBA para ClienteEventos

---
+++ src/cliente/ClienteEventosImpl.java
@@ -0,0 +1,13 @@
+package cliente;
+
+/**
+ * Cliente de Eventos
+ * @author brodock
+ */
+public class ClienteEventosImpl extends ClienteEventosPOA {
+
+    public boolean NovoAlerta(String evento) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
